3c5699a561ac0f0a5fc523938a0d06172853afcd
Alexander Diemand <codieplusplus@apax.net>
2022-11-30 23:47:49 +0100

    sort file blocks and number 'blockid'
    Signed-off-by: Alexander Diemand <codieplusplus@apax.net>

--- elykseer-utils/env.ml
+++ elykseer-utils/env.ml
@@ -7,6 +7,13 @@ let consolidate_files (bis : pl) : t =
   let fnames = List.map (fst) bis |> List.sort_uniq (compare) in
   List.fold_left (
     fun acc fname ->
-      let blocks = List.filter (fun fbi -> fname = (fst fbi)) bis |> List.map (snd) in
+                   (* list only blocks in this file *)
+      let blocks = List.filter (fun fbi -> fname = (fst fbi)) bis |> List.map (snd) |>
+                   (* sort blocks by filepos ascending *)
+                   List.sort (fun e1 e2 -> compare (Conversion.n2i (Assembly.filepos e1)) (Conversion.n2i (Assembly.filepos e2))) |>
+                   (* set increasing blockid *)
+                   List.mapi (fun i0 (e : Assembly.blockinformation) -> {e with blockid = Conversion.i2p (i0 + 1)}) |>
+                   (* reverse list *)
+                   List.rev in
       (fname, blocks) :: acc
     ) [] fnames
