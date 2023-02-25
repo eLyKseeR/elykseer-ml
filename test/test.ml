4d239a2ff49e0f7288b3c92656e91c17e3dfb4b3
Alexander Diemand <codieplusplus@apax.net>
2023-02-25 20:02:21 +0100

    extract keys for a list of files and the corresponding assemblies
    Signed-off-by: Alexander Diemand <codieplusplus@apax.net>

--- test/test.ml
+++ test/test.ml
@@ -1,5 +1,19 @@
-let () =
+
+let plain_tests () =
   let open Alcotest in
   run "LXR Assembly" [
     TestAssembly.test;
   ]
+let lwt_tests () =
+  let open Alcotest_lwt in
+  run "LXR Relkeys" [
+    TestRelkeys.test;
+  ]
+
+let tests () =
+  let () = plain_tests () in
+  lwt_tests ()
+ 
+let () =
+  let () = plain_tests () in
+  Lwt_main.run (tests ())
